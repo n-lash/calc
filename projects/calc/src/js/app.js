d34cf6640fa3cad58d76e52e241a22d7c4b5989c
n-lash <natalas111222@gmail.com>
2020-10-26 21:37:01 +0200

    Media queries added

--- projects/calc/src/js/app.js
+++ projects/calc/src/js/app.js
@@ -1,10 +1,16 @@
 import "./slider";
 
 
+$('.calc-menu-button').on('click', function () {
+    $('.calc-menu-button').toggleClass('active');
+    $('.calc-menu').toggleClass('active');
+});
+
+
 $('.form-select-box').chosen({width: "85%"});
 
 $('#resetBtn').on('click', function () {
     
     $('.form-select-box').val('').trigger('chosen:updated');
+    slider.noUiSlider.reset();
 });
-
