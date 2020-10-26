bcb6494b9a3f536bd60b741ac0054f58740334d1
n-lash <natalas111222@gmail.com>
2020-10-26 17:28:55 +0200

    Multiple select added

--- projects/calc/src/js/app.js
+++ projects/calc/src/js/app.js
@@ -1,1 +1,10 @@
 import "./slider";
+
+
+$('.form-select-box').chosen({width: "85%"});
+
+$('#resetBtn').on('click', function () {
+    
+    $('.form-select-box').val('').trigger('chosen:updated');
+});
+
